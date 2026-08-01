212168aa5bd22d8e38f46af6481e89f2b92169ae
IndeedItzGab <gabrielpbondoc@gmail.com>
2026-08-01 23:58:27 +0800

    api update

--- config-parser/modules/utils.mjs
+++ config-parser/modules/utils.mjs
@@ -82,7 +82,7 @@ export class Generator {
       }
 
       // Request from the image generator to generate the information from inputs
-      const response = await fetch("api/generate/index.py", {
+      const response = await fetch("https://config-parser-sepia.vercel.app/api/generate/index.py", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
